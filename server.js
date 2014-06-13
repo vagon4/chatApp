0bfb5c1dde57fe1bb815a5de45d838b3c0bbc927
Vaughan Ramsey <vagon4@gmail.com>
2014-06-13 09:08:47 +0000

    Adding some 404 and data helper functions

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
+
+function send404(res){
+	res.writeHead(404, {'Content-type': 'text/plain'});
+	res.write('Error 404: resource not found');
+	res.end();
+}
+
+function sendFile(res, fPath, fContents){
+	res.writeHead(
+		200,
+		{"Content-type": mime.lookup(path.basename(fPath))}
+	);
+	res.end(fContents);
+}
